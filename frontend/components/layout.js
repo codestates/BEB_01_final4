78d6fa4e41b443df0a319c5eac2e0440e63ef223
hyun2 <hks0284@gmail.com>
2022-01-06 01:38:04 +0900

    프론트 레이아웃 생성

---
+++ frontend/components/layout.js
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { AppShell, Burger, Button, Header, MediaQuery, Navbar, Text, useMantineTheme } from "@mantine/core";
+import Image from "next/image";
+import { Input } from "@mantine/core";
+import Link from "next/link";
+import styled from "styled-components";
+
+const CText = styled(Text)`
+  && {
+    margin: 0 15px;
+    cursor: pointer;
+  }
+`;
+
+const Layout = ({ children }) => {
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
+
+  return (
+    <AppShell
+      style={{ paddingRight: "calc(0px + 16px)" }}
+      // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
+      navbarOffsetBreakpoint="sm"
+      // fixed prop on AppShell will be automatically added to Header and Navbar
+      fixed
+      navbar={
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <Navbar
+            style={{ marginTop: "70px" }}
+            padding="md"
+            // Breakpoint at which navbar will be hidden if hidden prop is true
+            hiddenBreakpoint="sm"
+            // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
+            hidden={!opened}
+            // when viewport size is less than theme.breakpoints.sm navbar width is 100%
+            // viewport size > theme.breakpoints.sm – width is 200px
+            // viewport size > theme.breakpoints.lg – width is 300px
+            // width={{ sm: 200, lg: 300 }}
+            width={{ sm: 300, lg: 400 }}
+          >
+            <Text>Application navbar</Text>
+          </Navbar>
+        </MediaQuery>
+      }
+      header={
+        <Header height={70} padding="md">
+          {/* Handle other responsive styles with MediaQuery component or createStyles function */}
+          <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", height: "100%" }}>
+            <div style={{ display: "flex" }}>
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  style={{ alignSelf: "center" }}
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+
+              <div style={{ display: "flex", cursor: "pointer" }}>
+                <Image width="35" height="45" src="/images/gganbu.png" alt="" />
+                <Link href="/" passHref>
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      fontSize: "25px",
+                      marginLeft: "10px",
+                      alignSelf: "center",
+                    }}
+                  >
+                    NFT GGanbu
+                  </Text>
+                </Link>
+              </div>
+            </div>
+
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Input style={{ marginRight: "20px", width: "300px" }} variant="default" placeholder="Search" />
+
+              <div style={{ display: "flex" }}>
+                <Link href="/mint-nft" passHref>
+                  <CText>NFT 민트</CText>
+                </Link>
+                <Link href="/" passHref>
+                  <CText>메뉴 1</CText>
+                </Link>
+                <Link href="/" passHref>
+                  <CText>메뉴 2</CText>
+                </Link>
+              </div>
+            </div>
+
+            <Button variant="light" color="orange">
+              <Image width={28} height={28} src="https://docs.metamask.io/metamask-fox.svg" alt="" />
+              <span style={{ marginLeft: "10px" }}>지갑 연결</span>
+            </Button>
+          </div>
+        </Header>
+      }
+    >
+      <Text>{children}</Text>
+    </AppShell>
+  );
+};
+
+export default Layout;
