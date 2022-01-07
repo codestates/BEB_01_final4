b84a0496b504511b45fa3f0ca7cb98d8f1950a24
mindol8 <37296580+mindol8@users.noreply.github.com>
2022-01-07 16:09:47 +0900

    trade_수정2

--- contract/contracts/create_nft.sol
+++ contract/contracts/create_nft.sol
@@ -15,8 +15,8 @@ contract GGanbuCollection is ERC721URIStorage, Ownable {
     mapping(uint256 => bool) private auction;
     string private collectionURI;
 
-    event sell(address indexed contractAddr, address collection, uint256 tokenId, uint256 price);
-    event cancel(address indexed contractAddr, address collection, uint256 tokenId);
+    event _sell(address indexed contractAddr, address collection, uint256 tokenId, uint256 price);
+    event _cancel(address indexed contractAddr, address collection, uint256 tokenId);
 
     constructor(string memory collectionName, string memory collectionSymbol, string memory _collectionURI) ERC721(collectionName, collectionSymbol) {
        collectionURI = _collectionURI;
@@ -28,6 +28,10 @@ contract GGanbuCollection is ERC721URIStorage, Ownable {
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId,tokenURI);
         return newItemId;
+    }
+    
+    function getCollectionURI() public view returns(string memory){
+        return collectionURI;
     }
 
     function burn(uint256 tokenId) public onlyOwner returns (bool){     
@@ -44,17 +48,19 @@ contract GGanbuCollection is ERC721URIStorage, Ownable {
         address tradeContract = address(new trade(msg.sender,address(this),tokenId,price));//trade contract 생성
         _approve(tradeContract, tokenId);//tradeContract에서 transferFrom 사용할 수 있게 함
         //addOwnership(tradeContract);//거래 이후 nft 받는 사람에게 collection 접근 권한 주기위함. 거래완료 이후 삭ㅇ제예정
-        emit sell(tradeContract,address(this),tokenId,price);//event 발생
+        emit _sell(tradeContract,address(this),tokenId,price);//event 발생
         return tradeContract;
     }
 
-    function cancel(address contractAddr) public onlyOwner returns(bool){
+    function cancel(uint256 tokenId,address contractAddr) public  returns(bool){
+        require(msg.sender == ownerOf(tokenId),"Collection: This address is not owner");//해당 nft의 owner만 취소 할 수 있다.
         require(!auction[tokenId],"Collection: Can not sell this NFT");//판매중인지 검사
         //trade 호출
         //취소
         auction[tokenId] = false;
         //trade contract 비활성화
-        emit cancel(tradeContract,address(this),tokenId);//event 발생
+        emit _cancel(contractAddr,address(this),tokenId);//event 발생
+        return true;
     }
    
    function getIsSelling(uint256 tokenId)public view returns(bool){
