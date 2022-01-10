d5d876900afb2881bd914e373ec55dbbd7f4e612
hyun2 <hks0284@gmail.com>
2022-01-10 09:50:26 +0900

    프로필 아이콘 변경

--- frontend/components/profile.js
+++ frontend/components/profile.js
@@ -1,6 +1,7 @@
-import { Menu } from "@mantine/core";
+import { ActionIcon, Menu } from "@mantine/core";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { BiUserCircle } from "react-icons/bi";
 
 // const ProfileIcon = () => {
 //   return (
@@ -31,23 +32,26 @@ const Profile = () => {
     <Menu
       controlRefProp="ref"
       control={
-        <div
-          style={{
-            width: "36px",
-            height: "36px",
-            border: "2px solid rgb(229, 232, 235)",
-            borderRadius: "16px",
-          }}
-        >
-          <>
-            <Image className="profile-icon" src="/images/profile20.png" width="100%" height="100%" alt="" />
-            <style jsx global>{`
-              .profile-icon {
-                border-radius: 50%;
-              }
-            `}</style>
-          </>
-        </div>
+        // <div
+        //   style={{
+        //     width: "36px",
+        //     height: "36px",
+        //     border: "2px solid rgb(229, 232, 235)",
+        //     borderRadius: "16px",
+        //   }}
+        // >
+        //   <>
+        //     <Image className="profile-icon" src="/images/profile20.png" width="100%" height="100%" alt="" />
+        //     <style jsx global>{`
+        //       .profile-icon {
+        //         border-radius: 50%;
+        //       }
+        //     `}</style>
+        //   </>
+        // </div>
+        <ActionIcon size={36} style={{ cursor: "auto" }}>
+          <BiUserCircle style={{ width: "40px", height: "40px" }} />
+        </ActionIcon>
       }
       trigger="hover"
       delay={200}
