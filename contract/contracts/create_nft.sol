fbd6db3821c5b8ae9de8e9cdb6ac42329bb05372
autoininc <37296580+mindol8@users.noreply.github.com>
2022-01-12 14:16:24 +0900

    trade 최종

--- contract/contracts/create_nft.sol
+++ contract/contracts/create_nft.sol
@@ -15,63 +15,102 @@ contract GGanbuCollection is ERC721URIStorage, Ownable {
     mapping(uint256 => bool) private auction;
     string private collectionURI;
 
-    event _sell(address indexed contractAddr, address collection, uint256 tokenId, uint256 price);
-    event _cancel(address indexed contractAddr, address collection, uint256 tokenId);
-    
+    event _sell(
+        address indexed contractAddr,
+        address collection,
+        uint256 tokenId,
+        uint256 price
+    );
+    event _cancel(
+        address indexed contractAddr,
+        address collection,
+        uint256 tokenId
+    );
 
-    constructor(string memory collectionName, string memory collectionSymbol, string memory _collectionURI) ERC721(collectionName, collectionSymbol) {
-       collectionURI = _collectionURI;
+    constructor(
+        string memory collectionName,
+        string memory collectionSymbol,
+        string memory _collectionURI
+    ) ERC721(collectionName, collectionSymbol) {
+        collectionURI = _collectionURI;
     }
-    
-    function mintNFT(string memory tokenURI) public onlyOwner returns (uint256) {    
+
+    function mintNFT(string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId,tokenURI);
-       return newItemId;
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
     }
-    
-    function getCollectionURI() public view returns(string memory){
+
+    function getCollectionURI() public view returns (string memory) {
         return collectionURI;
     }
 
-    function burn(uint256 tokenId) public onlyOwner{     
-        require(msg.sender == ownerOf(tokenId),"Collection: This address is not owner");//해당 nft의 owner만 삭제 할 수 있다.  
+    function burn(uint256 tokenId) public onlyOwner {
+        require(
+            msg.sender == ownerOf(tokenId),
+            "Collection: This address is not owner"
+        ); //해당 nft의 owner만 삭제 할 수 있다.
         ERC721URIStorage._burn(tokenId);
         emit burnNft(tokenId);
-        
     }
-    function totalSupply()public view returns(uint256){
+
+    function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
 
-    function sell(uint256 tokenId, uint256 price) public{
-        require(msg.sender == ownerOf(tokenId),"Collection: This address is not owner");//해당 nft의 owner만 판매등록 할 수 있다.
-        require(!auction[tokenId],"Collection: Can not sell this NFT");//이미 판매중인지 검사
-        auction[tokenId] = true;        
-        address tradeContract = address(new trade(msg.sender,address(this),tokenId,price));//trade contract 생성
-        _approve(tradeContract, tokenId);//tradeContract에서 transferFrom 사용할 수 있게 함
+    function sell(uint256 tokenId, uint256 price) public {
+        require(
+            msg.sender == ownerOf(tokenId),
+            "Collection: This address is not owner"
+        ); //해당 nft의 owner만 판매등록 할 수 있다.
+        require(!auction[tokenId], "Collection: Can not sell this NFT"); //이미 판매중인지 검사
+        auction[tokenId] = true;
+        address tradeContract = address(
+            new trade(msg.sender, address(this), tokenId, price)
+        ); //trade contract 생성
+        _approve(tradeContract, tokenId); //tradeContract에서 transferFrom 사용할 수 있게 함
         //addOwnership(tradeContract);//거래 이후 nft 받는 사람에게 collection 접근 권한 주기위함. 거래완료 이후 삭ㅇ제예정
-        emit _sell(tradeContract,address(this),tokenId,price);//event 발생
-        
+        emit _sell(tradeContract, address(this), tokenId, price); //event 발생
+
         //getApproved()로 contract address 가져올 수 있다.
     }
 
-    function cancel(uint256 tokenId,address contractAddr) public {
-        require(msg.sender == ownerOf(tokenId),"Collection: This address is not owner");//해당 nft의 owner만 취소 할 수 있다.
-        require(auction[tokenId],"Collection: Can not sell this NFT");//판매중일때만 취소가능
-        if(contractAddr.balance != 0){
-
-        }
-        //취소(잔액이 0이 아니면 돌려주고 )
+    function cancel(uint256 tokenId, address payable contractAddr) public {
+        require(
+            msg.sender == ownerOf(tokenId),
+            "Collection: This address is not owner"
+        ); //해당 nft의 owner만 취소 할 수 있다.
+        require(auction[tokenId], "Collection: Can not sell this NFT"); //판매중일때만 취소가능
+        trade target = trade(contractAddr);
+        target.setInvalid(msg.sender); //취소
+        _approve(address(0x0), tokenId); //tradeContract에서 transferFrom 사용할 수 있게 함
         auction[tokenId] = false;
         //trade contract 비활성화
-        emit _cancel(contractAddr,address(this),tokenId);//event 발생
-        
+        emit _cancel(contractAddr, address(this), tokenId); //event 발생
     }
-   
-   function getIsSelling(uint256 tokenId)public view returns(bool){
-       return auction[tokenId] ;
-   }
-        
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        //solhint-disable-next-line max-line-length
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+
+        _transfer(from, to, tokenId);
+        auction[tokenId] = false;
+    }
+
+    function getIsSelling(uint256 tokenId) public view returns (bool) {
+        return auction[tokenId];
+    }
 }
---
+++ contract/index.js
@@ -0,0 +1,152 @@
+import Web3 from "web3";
+import abi from "./abi.js";
+import byteCode from "./bytecode.js";
+const contractAddr = "";//ropsten
+const account = "";
+//const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+const ropsten = ``;
+const web3 = new Web3(ropsten);
+const privateKey = "";
+
+const contract = new web3.eth.Contract(abi, contractAddr, { from: account });
+/**
+ //collection 생성할 때 마다 새로운 collection 배포하는 코드
+
+    var collectionName = ""  ;
+    var collectionSymbol ="" ;
+    var _collectionURI = ""  ;
+    var gganbucollectionContract = new web3.eth.Contract(abi);
+    var gganbucollection = gganbucollectionContract.deploy({
+     data: `0x${byteCode.object}`,
+      arguments: [
+          collectionName,
+          collectionSymbol,     
+          _collectionURI,
+     ]
+}).send({
+     from: web3.eth.accounts[0], 
+     gas: '4700000'
+   }, function (e, contract){
+    console.log(e, contract);
+    if (typeof contract.address !== 'undefined') {
+         console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
+    }
+ })
+
+ 주의!! .send이후 contract.address 값이 collection 주소랑 다를 수 있다. => 이런경우 보통 contract.to가 collection 주소
+ */
+const mint = async () => {
+    try {
+        const mintMethod = await contract.methods.mintNFT("test nft uri");
+        const encodedAbi = mintMethod.encodeABI();
+        const tx = {
+            from: account,
+            to: contractAddr,
+            gas: 200000,
+            data: encodedAbi
+        };
+        const signed = await web3.eth.accounts.signTransaction(tx, privateKey);
+        const tran = web3.eth.sendSignedTransaction(signed.rawTransaction);
+
+        tran.on('transactionHash', hash => {
+            console.log('hash');
+            console.log(hash);
+        });
+
+        tran.on('receipt', receipt => {
+            console.log('reciept');
+            console.log(receipt);
+        });
+
+        tran.on('error', console.error);
+
+    } catch (error) {
+        console.log(error);
+    }
+    //console.log(await contract.methods);
+
+
+}
+
+const _sell = async (tokenId, price) => {
+    //거래 등록
+    try {
+        const mintMethod = await contract.methods.sell(tokenId, web3.utils.toWei(String(price), "ether"));
+        const encodedAbi = mintMethod.encodeABI();
+        const tx = {
+            from: account,
+            to: contractAddr,
+            gas: 5000000,
+            data: encodedAbi
+        };
+        const signed = await web3.eth.accounts.signTransaction(tx, privateKey);
+        const tran = web3.eth.sendSignedTransaction(signed.rawTransaction);
+
+        tran.on('transactionHash', hash => {
+            console.log('hash');
+            console.log(hash);
+        });
+
+        tran.on('receipt', receipt => {
+            console.log('reciept');
+            console.log(receipt);
+        });
+
+        tran.on('error', console.error);
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+const total = async () => {
+    console.log(await contract.methods.totalSupply().call());
+}
+
+const isSell = async (tokenId) => {
+    console.log(await contract.methods.getIsSelling(tokenId).call());
+}
+const getOwnerOfNft = async (tokenId) => {
+    console.log(await contract.methods.ownerOf(tokenId).call());
+}
+const getTradeContract = async (tokenId) => {
+    console.log(await contract.methods.getApproved(tokenId).call());
+}
+
+const cancelSale = async (tokenId, tradecontract) => {
+    try {
+        const mintMethod = await contract.methods.cancel(tokenId, tradecontract);
+        const encodedAbi = mintMethod.encodeABI();
+        const tx = {
+            from: account,
+            to: contractAddr,
+            gas: 2000000,
+            data: encodedAbi
+        };
+        const signed = await web3.eth.accounts.signTransaction(tx, privateKey);
+        const tran = web3.eth.sendSignedTransaction(signed.rawTransaction);
+
+        tran.on('transactionHash', hash => {
+            console.log('hash');
+            console.log(hash);
+        });
+
+        tran.on('receipt', receipt => {
+            console.log('reciept');
+            console.log(receipt);
+        });
+
+        tran.on('error', console.error);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+//mint();
+_sell(1, 0.01);
+//isSell(1);
+//getOwnerOfNft(1);
+//cancelSale(1, tradeContract);
+//total();
