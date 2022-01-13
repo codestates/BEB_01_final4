88aa95ac4e9224254de37eccf86e47ca42513f09
hyun2 <hks0284@gmail.com>
2022-01-14 00:56:00 +0900

    NFT 소유자가 아닌 사용자가 sell 페이지에 접근 시 리다이렉트

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,11 +4,15 @@ import { MantineProvider } from "@mantine/core";
 import Layout, { connectWallet } from "../components/layout";
 import Web3 from "web3";
 import { useStore } from "../utils/store";
+import { useRouter } from "next/router";
+import { GGanbuCollection } from "../public/compiledContracts/GGanbuCollection";
+import axios from "axios";
 
 export default function App(props) {
   const { Component, pageProps } = props;
-  const setWeb3 = useStore((state) => state.setWeb3);
-  const [setAccount, setUser] = useStore((state) => [state.setAccount, state.setUser]);
+  const [web3, setWeb3] = useStore((state) => [state.web3, state.setWeb3]);
+  const [account, setAccount, setUser] = useStore((state) => [state.account, state.setAccount, state.setUser]);
+  const router = useRouter();
 
   useEffect(() => {
     if (typeof window.ethereum !== "undefined") {
@@ -25,6 +29,35 @@ export default function App(props) {
       }
     }
   }, []);
+
+  const handleRouteChange = async (url) => {
+    const splitted = url.split("/");
+
+    // splitted[2]는 symbol, splitted[3]는 tokenId
+    if (splitted[1] === "assets" && !isNaN(splitted[3]) && splitted[4] === "sell") {
+      const {
+        data: { data: nftsData },
+      } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/collections/${splitted[2]}/`, {
+        withCredentials: true,
+      });
+      // console.log(nftsData);
+      if (web3) {
+        const contract = await new web3.eth.Contract(GGanbuCollection.abi, nftsData?.contractAddress, {
+          from: account,
+        });
+        const nftOwner = await contract.methods.ownerOf(splitted[3]).call();
+        console.log(nftOwner, account);
+        if (nftOwner !== account) {
+          alert("NFT 소유자가 아닙니다.");
+          router.push("/");
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", handleRouteChange);
+  }, [web3]);
 
   return (
     <>
