9e59ec5fd915c14791aef610fa23c245bab8e4be
hyun2 <hks0284@gmail.com>
2022-01-20 15:56:08 +0900

    깐부 페이지 생성

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,5 @@
-import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import Image from "next/image";
-import Slide from "../components/slide";
 
 export default function Home() {
   return (
@@ -15,11 +12,6 @@ export default function Home() {
         showThumbs={false}
         width="100%"
       >
-        {/* <Slide key="0" image="https://venturebeat.com/wp-content/uploads/2021/03/article23-1.jpg?w=1200&strip=all" />
-        <Slide key="1" image="https://images.newindianexpress.com/uploads/user/imagelibrary/2021/11/27/w1200X800/Metaverse_is_Coming.jpg" />
-        <Slide key="2" image="/images/carousel3.jpeg" />
-        <Slide key="3" image="/images/carousel4.jpeg" /> */}
-
         <div>
           {/* <Image layout="fill" src="/images/carousel1.jpeg" alt="" /> */}
           <img src="/images/carousel1 (1).jpeg" alt="" />
