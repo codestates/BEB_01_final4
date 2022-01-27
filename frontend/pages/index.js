3133c28c99b4d9dc9ca572cfc42aeb9b1c8c4bcd
hyun2 <hks0284@gmail.com>
2022-01-28 02:11:56 +0900

    기타 버그 fix

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,6 +6,8 @@ import RankingCard from "../components/rankingCard";
 import { Button, Divider, SimpleGrid, Text } from "@mantine/core";
 import styled from "styled-components";
 import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const CImage = styled(Image)`
   border-top-left-radius: 10px;
@@ -18,14 +20,20 @@ const Main = () => {
   const [topUsers, setTopUsers] = useState(null);
 
   const getRanking = async () => {
-    const { data: { data: ranking },
-    } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/main/trades?sort=price-high&status=completed`);
-    setRanking(ranking);
+    try {
+      const {
+        data: { data: ranking },
+      } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/main/trades?sort=price-high&status=completed`);
+      setRanking(ranking);
 
-    const { data: { data: topUsers },
-    } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/main/topUser`);
-    setTopUsers(topUsers);
-
+      const {
+        data: { data: topUsers },
+      } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/main/topUser`);
+      // console.log(topUsers);
+      setTopUsers(topUsers);
+    } catch (e) {
+      console.log(e);
+    }
   };
   useEffect(() => {
     getRanking();
@@ -34,19 +42,50 @@ const Main = () => {
   return (
     <main>
       {/* <header style={{backgroundImage: "url(/images/main.png)", backgroundPosition: "center top", backgroundRepeat: "no-repeat", position: "relative" }}> */}
+      <div>
+        <Carousel
+          style={{ height: "500px" }}
+          emulateTouch={true}
+          autoPlay={true}
+          showArrows={true}
+          showThumbs={false}
+          width="100%"
+        >
+          <div>
+            {/* <Image layout="fill" src="/images/carousel1.jpeg" alt="" /> */}
+            <img src="/images/carousel1 (1).jpeg" alt="" />
+          </div>
+          <div>
+            {/* <Image layout="fill" src="/images/carousel2.png" alt="" /> */}
+            <img src="/images/carousel2 (1).png" alt="" />
+          </div>
+          <div>
+            {/* <Image layout="fill" src="/images/carousel3.jpeg" alt="" /> */}
+            <img src="/images/carousel3 (1).jpeg" alt="" />
+          </div>
+          <div>
+            {/* <Image layout="fill" src="/images/carousel4.jpeg" alt="" /> */}
+            <img src="/images/carousel4 (1).png" alt="" />
+          </div>
+        </Carousel>
+      </div>
       <div style={{ textAlign: "center" }}>
         <img style={{ marginTop: "40px", height: "500px", position: "center" }} src="/images/main.png" />
       </div>
-      <div style={{ paddingTop: "1oopx", color: "#9986EE", fontSize: "70px", textAlign: "center", textShadow: "3px 3px 3px #A0A0C8" }}>
+      <div
+        style={{
+          paddingTop: "1oopx",
+          color: "#9986EE",
+          fontSize: "70px",
+          textAlign: "center",
+          textShadow: "3px 3px 3px #A0A0C8",
+        }}
+      >
         NFT GGANBU
       </div>
-      <div style={{ marginTop: "12px", fontSize: "30px", textAlign: "center" }}>
-        NFT, 더 이상 혼자 사지 마세요
-      </div>
+      <div style={{ marginTop: "12px", fontSize: "30px", textAlign: "center" }}>NFT, 더 이상 혼자 사지 마세요</div>
       <Divider style={{ margin: "60px 0 60px 0" }} />
-      <div style={{ marginTop: "15px", fontSize: "30px", textAlign: "center" }}>
-        가장 비싸게 팔린 NFT TOP 3
-      </div>
+      <div style={{ marginTop: "15px", fontSize: "30px", textAlign: "center" }}>가장 비싸게 팔린 NFT TOP 3</div>
       <SimpleGrid
         style={{ padding: "0 80px" }}
         cols={3}
@@ -63,9 +102,7 @@ const Main = () => {
         })}
       </SimpleGrid>
       <Divider style={{ margin: "60px 0 60px 0" }} />
-      <div style={{ marginTop: "15px", fontSize: "30px", textAlign: "center" }}>
-        TOP USER 3
-      </div>
+      <div style={{ marginTop: "15px", fontSize: "30px", textAlign: "center" }}>TOP USER 3</div>
       <SimpleGrid
         style={{ padding: "0 80px" }}
         cols={3}
@@ -76,14 +113,12 @@ const Main = () => {
         ]}
       >
         {topUsers?.map((user, i) => {
-          return user.imageURI === null ? null : (
-            <RankingCard key={i} rankInfo={user} index={i} category={"user"} />
-          );
+          return <RankingCard key={i} rankInfo={user} index={i} category={"user"} />;
         })}
       </SimpleGrid>
       <Divider style={{ margin: "60px 0 60px 0" }} />
     </main>
   );
-}
+};
 
 export default Main;
